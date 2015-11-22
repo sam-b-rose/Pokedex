229bcd383698a998bd7e2ba3927159ce57e1b2ba
Sam Rose <samrose3@gmail.com>
2015-11-21 22:48:31 -0500

    created main collection view

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Sam Rose on 11/21/15.
+//  Copyright © 2015 samrose3. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        _name = name
+        _pokedexId = pokedexId
+    }
+}
